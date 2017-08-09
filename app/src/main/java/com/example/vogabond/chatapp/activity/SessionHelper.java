052505a50004502223f6b6025621538472fca1a1
1771092472 <1771092472@qq.com>
2017-08-09 16:34:31 +0800

    添加用户关系托管功能

---
+++ app/src/main/java/com/example/vogabond/chatapp/activity/SessionHelper.java
@@ -0,0 +1,12 @@
+package com.example.vogabond.chatapp.helper.activity;
+
+/**
+ * Created by dell on 2017/8/7.
+ */
+
+class SessionHelper {
+
+    public static void startP2PSession(RobotProfileActivity robotProfileActivity, String robotAccount) {
+
+    }
+}
