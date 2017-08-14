ee70db26d2199b42f0753e5f9578e610b755b372
Liangxing4910 <644551024@qq.com>
2017-08-14 14:31:46 +0800

    修改了MyAPPlication等
    修改了MyAPPlication增加了几个辅助类

--- app/src/main/java/com/example/vogabond/chatapp/MyApplication.java
+++ app/src/main/java/com/example/vogabond/chatapp/MyApplication.java
@@ -1,93 +1,322 @@
 package com.example.vogabond.chatapp;
 
 import android.app.Application;
-import android.graphics.Bitmap;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
 import android.os.Environment;
+import android.text.TextUtils;
+import android.util.Log;
 
+import com.example.vogabond.chatapp.avchat.AVChatProfile;
+import com.example.vogabond.chatapp.avchat.activity.AVChatActivity;
+import com.example.vogabond.chatapp.avchat.receiver.PhoneCallStateEnum;
+import com.example.vogabond.chatapp.avchat.receiver.PhoneCallStateObserver;
+import com.example.vogabond.chatapp.contact.ContactHelper;
 import com.example.vogabond.chatapp.main.activity.WelcomeActivity;
+import com.example.vogabond.chatapp.preference.Preferences;
+import com.example.vogabond.chatapp.preference.UserPreferences;
+import com.example.vogabond.chatapp.session.SessionHelper;
+import com.netease.nim.uikit.NimUIKit;
+import com.netease.nim.uikit.common.util.log.LogUtil;
+import com.netease.nim.uikit.contact.core.query.PinYin;
+import com.netease.nim.uikit.custom.DefaultUserInfoProvider;
 import com.netease.nim.uikit.session.viewholder.MsgViewHolderThumbBase;
 import com.netease.nimlib.sdk.NIMClient;
+import com.netease.nimlib.sdk.NimStrings;
+import com.netease.nimlib.sdk.Observer;
 import com.netease.nimlib.sdk.SDKOptions;
+import com.netease.nimlib.sdk.ServerAddresses;
 import com.netease.nimlib.sdk.StatusBarNotificationConfig;
 import com.netease.nimlib.sdk.auth.LoginInfo;
-import com.netease.nimlib.sdk.msg.constant.SessionTypeEnum;
-import com.netease.nimlib.sdk.uinfo.UserInfoProvider;
+import com.netease.nimlib.sdk.avchat.AVChatManager;
+import com.netease.nimlib.sdk.avchat.constant.AVChatControlCommand;
+import com.netease.nimlib.sdk.avchat.model.AVChatAttachment;
+import com.netease.nimlib.sdk.avchat.model.AVChatData;
+import com.netease.nimlib.sdk.msg.MessageNotifierCustomization;
+import com.netease.nimlib.sdk.msg.MsgService;
+import com.netease.nimlib.sdk.msg.model.IMMessage;
+import com.netease.nimlib.sdk.rts.RTSManager;
+import com.netease.nimlib.sdk.rts.model.RTSData;
+import com.netease.nimlib.sdk.team.constant.TeamFieldEnum;
+import com.netease.nimlib.sdk.team.model.IMMessageFilter;
+import com.netease.nimlib.sdk.team.model.UpdateTeamAttachment;
+
+import java.util.Map;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by Vogabond on 2017/8/4.
  */
 
 public class MyApplication extends Application{
+
     @Override
     public void onCreate() {
-        NIMClient.init(this, loginInfo(), options());
+        MyCache.setContext(this);
+        NIMClient.init(this, getLoginInfo(), getOptions());
+//        ExtraOptions.provide();
+        // crash handler
+//        AppCrashHandler.getInstance(this);
+
+        if (inMainProcess()) {
+
+            // init pinyin
+            PinYin.init(this);
+            PinYin.validate();
+
+            // 初始化UIKit模块
+            initUIKit();
+
+            // 注册通知消息过滤器
+            registerIMMessageFilter();
+
+            // 初始化消息提醒
+            NIMClient.toggleNotification(UserPreferences.getNotificationToggle());
+
+            // 注册网络通话来电
+            registerAVChatIncomingCallObserver(true);
+
+            // 注册白板会话
+//            registerRTSIncomingObserver(true);
+
+            // 注册语言变化监听
+            registerLocaleReceiver(true);
+
+//            OnlineStateEventManager.init();
+        }
     }
 
-    // 如果返回值为 null，则全部使用默认参数。
-    protected SDKOptions options() {
+    private LoginInfo getLoginInfo() {
+        String account = Preferences.getUserAccount();
+        String token = Preferences.getUserToken();
+
+        if (!TextUtils.isEmpty(account) && !TextUtils.isEmpty(token)) {
+            MyCache.setAccount(account.toLowerCase());
+            return new LoginInfo(account, token);
+        } else {
+            return null;
+        }
+    }
+
+    private SDKOptions getOptions() {
         SDKOptions options = new SDKOptions();
 
-        // 如果将新消息通知提醒托管给 SDK 完成，需要添加以下配置。否则无需设置。
+        // 如果将新消息通知提醒托管给SDK完成，需要添加以下配置。
+        initStatusBarNotificationConfig(options);
+
+        // 配置保存图片，文件，log等数据的目录
+        options.sdkStorageRootPath = Environment.getExternalStorageDirectory() + "/" + getPackageName() + "/nim";
+
+        // 配置数据库加密秘钥
+        options.databaseEncryptKey = "NETEASE";
+
+        // 配置是否需要预下载附件缩略图
+        options.preloadAttach = true;
+
+        // 配置附件缩略图的尺寸大小，
+        options.thumbnailSize = MsgViewHolderThumbBase.getImageMaxEdge();
+
+        // 用户信息提供者
+        options.userInfoProvider = new DefaultUserInfoProvider(this);
+
+        // 定制通知栏提醒文案（可选，如果不定制将采用SDK默认文案）
+        options.messageNotifierCustomization = messageNotifierCustomization;
+
+        // 在线多端同步未读数
+        options.sessionReadAck = true;
+
+        // 云信私有化配置项
+        configServerAddress(options);
+
+        return options;
+    }
+
+    private void configServerAddress(final SDKOptions options) {
+        String appKey = PrivatizationConfig.getAppKey();
+        if (!TextUtils.isEmpty(appKey)) {
+            options.appKey = appKey;
+        }
+
+        ServerAddresses serverConfig = PrivatizationConfig.getServerAddresses();
+        if (serverConfig != null) {
+            options.serverConfig = serverConfig;
+        }
+    }
+
+    private void initStatusBarNotificationConfig(SDKOptions options) {
+        // load 应用的状态栏配置
+        StatusBarNotificationConfig config = loadStatusBarNotificationConfig();
+
+        // load 用户的 StatusBarNotificationConfig 设置项
+        StatusBarNotificationConfig userConfig = UserPreferences.getStatusConfig();
+        if (userConfig == null) {
+            userConfig = config;
+        } else {
+            // 新增的 UserPreferences 存储项更新，兼容 3.4 及以前版本
+            // 新增 notificationColor 存储，兼容3.6以前版本
+            // APP默认 StatusBarNotificationConfig 配置修改后，使其生效
+            userConfig.notificationEntrance = config.notificationEntrance;
+            userConfig.notificationFolded = config.notificationFolded;
+            userConfig.notificationColor = getResources().getColor(R.color.color_blue_3a9efb);
+        }
+        // 持久化生效
+        UserPreferences.setStatusConfig(userConfig);
+        // SDK statusBarNotificationConfig 生效
+        options.statusBarNotificationConfig = userConfig;
+    }
+
+    // 这里开发者可以自定义该应用初始的 StatusBarNotificationConfig
+    private StatusBarNotificationConfig loadStatusBarNotificationConfig() {
         StatusBarNotificationConfig config = new StatusBarNotificationConfig();
-        config.notificationEntrance = WelcomeActivity.class; // 点击通知栏跳转到该Activity
-        config.notificationSmallIconId = R.mipmap.ic_stat_notify_msg;
+        // 点击通知需要跳转到的界面
+        config.notificationEntrance = WelcomeActivity.class;
+        config.notificationSmallIconId = R.drawable.ic_stat_notify_msg;
+        config.notificationColor = getResources().getColor(R.color.color_blue_3a9efb);
+        // 通知铃声的uri字符串
+        config.notificationSound = "android.resource://com.netease.nim.demo/raw/msg";
+
         // 呼吸灯配置
         config.ledARGB = Color.GREEN;
         config.ledOnMs = 1000;
         config.ledOffMs = 1500;
-        // 通知铃声的uri字符串
-        config.notificationSound = "android.resource://com.netease.nim.demo/raw/msg";
-        options.statusBarNotificationConfig = config;
 
-        // 配置保存图片，文件，log 等数据的目录
-        // 如果 options 中没有设置这个值，SDK 会使用下面代码示例中的位置作为 SDK 的数据目录。
-        // 该目录目前包含 log, file, image, audio, video, thumb 这6个目录。
-        // 如果第三方 APP 需要缓存清理功能， 清理这个目录下面个子目录的内容即可。
-        String sdkPath = Environment.getExternalStorageDirectory() + "/" + getPackageName() + "/nim";
-        options.sdkStorageRootPath = sdkPath;
-
-        // 配置是否需要预下载附件缩略图，默认为 true
-        options.preloadAttach = true;
-
-        // 配置附件缩略图的尺寸大小。表示向服务器请求缩略图文件的大小
-        // 该值一般应根据屏幕尺寸来确定， 默认值为 Screen.width / 2
-        options.thumbnailSize = MsgViewHolderThumbBase.getImageMaxEdge();
-
-        // 用户资料提供者, 目前主要用于提供用户资料，用于新消息通知栏中显示消息来源的头像和昵称
-        options.userInfoProvider = new UserInfoProvider() {
-            @Override
-            public UserInfo getUserInfo(String account) {
-                return null;
-            }
-
-            @Override
-            public int getDefaultIconResId() {
-                return R.mipmap.t_avchat_avatar_default;
-            }
-
-            @Override
-            public Bitmap getTeamIcon(String tid) {
-                return null;
-            }
-
-            @Override
-            public Bitmap getAvatarForMessageNotifier(String account) {
-                return null;
-            }
-
-            @Override
-            public String getDisplayNameForMessageNotifier(String account, String sessionId,
-                                                           SessionTypeEnum sessionType) {
-                return null;
-            }
-        };
-        return options;
+        // save cache，留做切换账号备用
+        MyCache.setNotificationConfig(config);
+        return config;
     }
 
-    // 如果已经存在用户登录信息，返回LoginInfo，否则返回null即可
-    protected LoginInfo loginInfo() {
-        return null;
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
     }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+
+    public boolean inMainProcess() {
+        String packageName = getPackageName();
+        String processName = SystemUtil.getProcessName(this);
+        return packageName.equals(processName);
+    }
+
+    /**
+     * 通知消息过滤器（如果过滤则该消息不存储不上报）
+     */
+    private void registerIMMessageFilter() {
+        NIMClient.getService(MsgService.class).registerIMMessageFilter(new IMMessageFilter() {
+            @Override
+            public boolean shouldIgnore(IMMessage message) {
+                if (UserPreferences.getMsgIgnore() && message.getAttachment() != null) {
+                    if (message.getAttachment() instanceof UpdateTeamAttachment) {
+                        UpdateTeamAttachment attachment = (UpdateTeamAttachment) message.getAttachment();
+                        for (Map.Entry<TeamFieldEnum, Object> field : attachment.getUpdatedFields().entrySet()) {
+                            if (field.getKey() == TeamFieldEnum.ICON) {
+                                return true;
+                            }
+                        }
+                    } else if (message.getAttachment() instanceof AVChatAttachment) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+    }
+
+    private void registerAVChatIncomingCallObserver(boolean register) {
+        AVChatManager.getInstance().observeIncomingCall(new Observer<AVChatData>() {
+            @Override
+            public void onEvent(AVChatData data) {
+                String extra = data.getExtra();
+                Log.e("Extra", "Extra Message->" + extra);
+                if (PhoneCallStateObserver.getInstance().getPhoneCallState() != PhoneCallStateEnum.IDLE
+                        || AVChatProfile.getInstance().isAVChatting()
+//                        || TeamAVChatHelper.sharedInstance().isTeamAVChatting()
+                        || AVChatManager.getInstance().getCurrentChatId() != 0) {
+                    LogUtil.i(TAG, "reject incoming call data =" + data.toString() + " as local phone is not idle");
+                    AVChatManager.getInstance().sendControlCommand(data.getChatId(), AVChatControlCommand.BUSY, null);
+                    return;
+                }
+                // 有网络来电打开AVChatActivity
+                AVChatProfile.getInstance().setAVChatting(true);
+                AVChatProfile.getInstance().launchActivity(data, AVChatActivity.FROM_BROADCASTRECEIVER);
+            }
+        }, register);
+    }
+
+    private void registerLocaleReceiver(boolean register) {
+        if (register) {
+            updateLocale();
+            IntentFilter filter = new IntentFilter(Intent.ACTION_LOCALE_CHANGED);
+            registerReceiver(localeReceiver, filter);
+        } else {
+            unregisterReceiver(localeReceiver);
+        }
+    }
+
+    private BroadcastReceiver localeReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_LOCALE_CHANGED)) {
+                updateLocale();
+            }
+        }
+    };
+
+    private void updateLocale() {
+        NimStrings strings = new NimStrings();
+        strings.status_bar_multi_messages_incoming = getString(R.string.nim_status_bar_multi_messages_incoming);
+        strings.status_bar_image_message = getString(R.string.nim_status_bar_image_message);
+        strings.status_bar_audio_message = getString(R.string.nim_status_bar_audio_message);
+        strings.status_bar_custom_message = getString(R.string.nim_status_bar_custom_message);
+        strings.status_bar_file_message = getString(R.string.nim_status_bar_file_message);
+        strings.status_bar_location_message = getString(R.string.nim_status_bar_location_message);
+        strings.status_bar_notification_message = getString(R.string.nim_status_bar_notification_message);
+        strings.status_bar_ticker_text = getString(R.string.nim_status_bar_ticker_text);
+        strings.status_bar_unsupported_message = getString(R.string.nim_status_bar_unsupported_message);
+        strings.status_bar_video_message = getString(R.string.nim_status_bar_video_message);
+        strings.status_bar_hidden_message_content = getString(R.string.nim_status_bar_hidden_msg_content);
+        NIMClient.updateStrings(strings);
+    }
+
+    private void initUIKit() {
+        // 初始化，使用 uikit 默认的用户信息提供者
+        NimUIKit.init(this);
+
+        // 设置地理位置提供者。如果需要发送地理位置消息，该参数必须提供。如果不需要，可以忽略。
+//        NimUIKit.setLocationProvider(new NimDemoLocationProvider());
+
+        // 会话窗口的定制初始化。
+        SessionHelper.init();
+
+        // 通讯录列表定制初始化
+        ContactHelper.init();
+
+        // 添加自定义推送文案以及选项，请开发者在各端（Android、IOS、PC、Web）消息发送时保持一致，以免出现通知不一致的情况
+        // NimUIKit.CustomPushContentProvider(new DemoPushContentProvider());
+
+//        NimUIKit.setOnlineStateContentProvider(new DemoOnlineStateContentProvider());
+    }
+
+    private MessageNotifierCustomization messageNotifierCustomization = new MessageNotifierCustomization() {
+        @Override
+        public String makeNotifyContent(String nick, IMMessage message) {
+            return null; // 采用SDK默认文案
+        }
+
+        @Override
+        public String makeTicker(String nick, IMMessage message) {
+            return null; // 采用SDK默认文案
+        }
+    };
+
+
+
 
 }
