ef1d20deffad46b2493cc6d7d30abd2b74097f2c
qin <qintian74@gmail.com>
2017-08-19 20:32:11 +0800

    修复音视频聊天

---
+++ app/src/main/java/com/example/vogabond/chatapp/discovery/NewContentActivity.java
@@ -0,0 +1,11 @@
+package com.example.vogabond.chatapp.discovery;
+
+import com.example.vogabond.chatapp.activity.UI;
+
+/**
+ * Created by Selet on 2017/8/19 0019.
+ */
+
+public class NewContentActivity extends UI {
+
+}
